6549ec64fbeb2a3a5997a48d3d17a318c3888cdc
Toby <598354657@qq.com>
2017-03-17 15:49:58 +0800

    Insert to existing table function added

--- Database_Insert.py
+++ Database_Insert.py
@@ -1,4 +1,5 @@
 import MySQLdb
+import warnings
 
 
 # The table will start with a DATETIME column, since we assumed you use pandas to analyse time-series data
@@ -68,20 +69,23 @@ class DatabaseInsert(Database):
         self.sub_key = sub_key
         self.table_add = list()
         self.columns_add_list = list()
+        self.columns_status = False
 
-    def create_table(self, cols):
+    def set_columns(self, cols, create=False):
         if len(cols) < 800:
-            if self.table_name in self.table_list:
-                print "Table %s already existed" % self.table_name
-            else:
-                info = self.login_information
-                my_connect = MySQLdb.connect(info[0], info[1], info[2], info[3], info[4])
-                cursor = my_connect.cursor()
-                sql_command = create_table_command(cols, self.table_name, self.sub_key)
-                cursor.execute(sql_command)
-                my_connect.close()
-                cursor.close()
-                print "Table %s created" % self.table_name
+            if create:
+                if self.table_name in self.table_list:
+                    warn_message = "Table %s Already Existed" % self.table_name
+                    warnings.warn(warn_message, UserWarning)
+                else:
+                    info = self.login_information
+                    my_connect = MySQLdb.connect(info[0], info[1], info[2], info[3], info[4])
+                    cursor = my_connect.cursor()
+                    sql_command = create_table_command(cols, self.table_name, self.sub_key)
+                    cursor.execute(sql_command)
+                    my_connect.close()
+                    cursor.close()
+                    print "Table %s Created" % self.table_name
             self.table_add.append(self.table_name)
             self.columns_add_list.append(cols)
         else:
@@ -94,57 +98,66 @@ class DatabaseInsert(Database):
             start = 0
             end = 800
             for table_i in potential_name:
-                if table_i in self.table_list:
-                    print "Table %s already existed" % table_i
-                    col_add_i = cols[start:end]
-                else:
-                    info = self.login_information
-                    my_connect = MySQLdb.connect(info[0], info[1], info[2], info[3], info[4])
-                    cursor = my_connect.cursor()
-                    col_add_i = cols[start:end]
-                    sql_command = create_table_command(col_add_i, table_i, self.sub_key)
-                    cursor.execute(sql_command)
-                    my_connect.close()
-                    cursor.close()
-                    print "Table %s created" % table_i
+                col_add_i = cols[start:end]
+                if create:
+                    if table_i in self.table_list:
+                        warn_message = "Table %s Already Existed" % self.table_name
+                        warnings.warn(warn_message, UserWarning)
+                    else:
+                        info = self.login_information
+                        my_connect = MySQLdb.connect(info[0], info[1], info[2], info[3], info[4])
+                        cursor = my_connect.cursor()
+                        sql_command = create_table_command(col_add_i, table_i, self.sub_key)
+                        cursor.execute(sql_command)
+                        my_connect.close()
+                        cursor.close()
+                        print "Table %s Created" % table_i
                 self.table_add.append(table_i)
                 self.columns_add_list.append(col_add_i)
                 start += 800
                 end += 800
+        print "Columns Names Received!"
+        self.columns_status = True
 
     def insert_data(self, data_input, sub_key_data=False):
-        info = self.login_information
-        my_connect = MySQLdb.connect(info[0], info[1], info[2], info[3], info[4])
-        cursor = my_connect.cursor()
-        if self.sub_key:
-            for i in range(len(self.table_add)):
-                sql_command = "INSERT INTO %s (`DATETIME`, `%s`" % (self.table_add[i], self.sub_key)
-                col_i_list = self.columns_add_list[i]
-                date_data = list(data_input.index)
-                sub_key_column = [sub_key_data] * len(data_input)
-                main_data = map(list, zip(*data_input[col_i_list].values))
-                data_insert = [date_data] + [sub_key_column] + main_data
-                data_insert = map(list, zip(*data_insert))
+        validation = self.columns_status
+        if validation:
+            info = self.login_information
+            my_connect = MySQLdb.connect(info[0], info[1], info[2], info[3], info[4])
+            cursor = my_connect.cursor()
+            if self.sub_key:
+                for i in range(len(self.table_add)):
+                    sql_command = "INSERT INTO %s (`DATETIME`, `%s`" % (self.table_add[i], self.sub_key)
+                    col_i_list = self.columns_add_list[i]
+                    date_data = list(data_input.index)
+                    sub_key_column = [sub_key_data] * len(data_input)
+                    main_data = map(list, zip(*data_input[col_i_list].values))
+                    data_insert = [date_data] + [sub_key_column] + main_data
+                    data_insert = map(list, zip(*data_insert))
 
-                for col_i in col_i_list:
-                    sql_command = "%s, `%s`" % (sql_command, col_i)
-                sql_command += ") VALUES (%s" + ", %s" * (len(col_i_list) + 1) + ")"
-                cursor.executemany(sql_command, data_insert)
-                my_connect.commit()
+                    for col_i in col_i_list:
+                        sql_command = "%s, `%s`" % (sql_command, col_i)
+                    sql_command += ") VALUES (%s" + ", %s" * (len(col_i_list) + 1) + ")"
+                    cursor.executemany(sql_command, data_insert)
+                    my_connect.commit()
+            else:
+                for i in range(len(self.table_add)):
+                    sql_command = "INSERT INTO %s (`DATETIME`" % self.table_add[i]
+                    col_i_list = self.columns_add_list[i]
+                    date_data = list(data_input.index)
+                    main_data = map(list, zip(*data_input[col_i_list].values))
+                    data_insert = [date_data] + main_data
+                    data_insert = map(list, zip(*data_insert))
+
+                    for col_i in col_i_list:
+                        sql_command = "%s, `%s`" % (sql_command, col_i)
+                    sql_command += ") VALUES (%s" + ", %s" * len(col_i_list) + ")"
+                    cursor.executemany(sql_command, data_insert)
+                    my_connect.commit()
+            my_connect.close()
+            cursor.close()
+            print "Insert Complete!"
         else:
-            for i in range(len(self.table_add)):
-                sql_command = "INSERT INTO %s (`DATETIME`" % self.table_add[i]
-                col_i_list = self.columns_add_list[i]
-                date_data = list(data_input.index)
-                main_data = map(list, zip(*data_input[col_i_list].values))
-                data_insert = [date_data] + main_data
-                data_insert = map(list, zip(*data_insert))
-
-                for col_i in col_i_list:
-                    sql_command = "%s, `%s`" % (sql_command, col_i)
-                sql_command += ") VALUES (%s" + ", %s" * len(col_i_list) + ")"
-                cursor.executemany(sql_command, data_insert)
-                my_connect.commit()
-        my_connect.close()
-        cursor.close()
+            warn_message = "Invalid Insertion, Mission Failed"
+            warnings.warn(warn_message, UserWarning)
 
--- example.py
+++ example.py
@@ -10,17 +10,25 @@ login = ["host", "user", "password", "database", "port"]
 dates = pandas.date_range('20130101', periods=6)
 data_input = pandas.DataFrame(numpy.random.randn(6, 4), index=dates,columns=list('ABCD'))
 
-# Create table and insert data, (with sub_key)
+# Create table and insert data, and create a new table (with sub_key)
 cols = data_input.columns
 table_name = "table_test"
 sub_key = "Name"
 dbm = DatabaseInsert(login, table_name, sub_key)
-dbm.create_table(cols)
+dbm.set_columns(cols, create=True)
 dbm.insert_data(data_input, "Sunny")
 
 # Create table and insert data, (without sub_key)
 cols = data_input.columns
 table_name = "table_test"
 dbm = DatabaseInsert(login, table_name)
-dbm.create_table(cols)
+dbm.set_columns(cols, create=True)
 dbm.insert_data(data_input)
+
+# Create table and insert data, append to an existing table (with sub_key)
+cols = data_input.columns
+table_name = "table_test"
+sub_key = "Name"
+dbm = DatabaseInsert(login, table_name, sub_key)
+dbm.set_columns(cols, create=False)
+dbm.insert_data(data_input, "Cloudy")
